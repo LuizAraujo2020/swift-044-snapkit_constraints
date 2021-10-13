756b68d568a53aa24e249e0307ff074ae8c4115e
Luiz Araujo <luizcarlos_bsb2006@hotmail.com>
2021-10-13 08:29:41 -0300

    :gift: Getting Started

---
+++ README.md
@@ -0,0 +1,4 @@
+#  Swift #44
+
+# Autolayout with SnapKit
+## Tutorial: https://www.raywenderlich.com/3225401-snapkit-for-ios-constraints-in-a-snap
--- SnappyQuiz-iOS/Final/SnappyQuiz/Questions.swift
+++ SnappyQuiz-iOS/Final/SnappyQuiz/Questions.swift
@@ -26,16 +26,27 @@
 /// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 /// THE SOFTWARE.
 
-import UIKit
+import Foundation
 
-@UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
-  var window: UIWindow?
-  
-  func applicationDidFinishLaunching(_ application: UIApplication) {
-    let appearance = UINavigationBarAppearance()
-    appearance.backgroundColor = UIColor(red: 1, green: 108.0/255, blue: 0, alpha: 1.0)
-    UINavigationBar.appearance().scrollEdgeAppearance = appearance
-    UINavigationBar.appearance().standardAppearance = appearance
-  }
+struct Question {
+  let title: String
+  let isTrue: Bool
 }
+
+let questions = [
+  ("As far as has ever been reported, no-one has ever seen an ostrich bury its head in the sand.", true),
+  ("Approximately one quarter of human bones are in the feet.", true),
+  ("Popeye’s nephews were called Peepeye, Poopeye, Pipeye and Pupeye.", true),
+  ("In ancient Rome, a special room called a vomitorium was available for diners to purge food in during meals.", false),
+  ("The average person will shed 10 pounds of skin during their lifetime.", false),
+  ("The Great Wall Of China is visible from the moon.", false),
+  ("Virtually all Las Vegas gambling casinos ensure that they have no clocks.", true),
+  ("Risotto is a common Italian rice dish.", true),
+  ("The prefix \"mega-\" represents one million.", true),
+  ("The \"Forbidden City\" is in Beijing.", true),
+  ("Hurricanes and typhoons are the same thing.", true),
+  ("In Shakespeare's play, Hamlet commits suicide.", false),
+  ("An American was the first man in space.", false),
+  ("The \"China Syndrome\" is what hostages experience when they begin to feel empathy for their captors.", false),
+  ("Other than water, coffee is the world's most popular drink.", true)
+].map(Question.init)
